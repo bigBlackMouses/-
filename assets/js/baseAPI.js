0dcaad86ebe857afd8684681e55c4769995b1227
bigBlackMouses <469471457@qq.com>
2022-05-21 10:14:44 +0800

    完成首页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,22 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter((option) => {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = `http://www.liulongbin.top:3007` + option.url;
-  });
+$.ajaxPrefilter((options) => {
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  options.url = `http://www.liulongbin.top:3007` + options.url;
+  // 在请求之前给有权限的接口注入 token
+  if (options.url.includes("/my/")){
+    options.headers = {
+      Authorization: localStorage.getItem("token"),
+    }
+  }
+
+  // 统一处理权限问题
+  options.complete = (res) => {
+    // console.log(res);
+    if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
+        localStorage.removeItem("token");
+        location.href = "/login.html";
+    }
+}
+});
